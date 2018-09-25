88770b9be3cae853491273b8fc6b877c81ef8c14
Clint Johnson <CJohnson@illuminatetraining.local>
2018-09-25 14:55:39 -0400

    Added where:

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://ww.flickr.com/photos/big');
     this.nicknames = ko.observableArray(["FirstNick", "SecondNick", "ThirdNick"]);
-
-
 
     this.title = ko.computed(function() {
         var self = this;
@@ -16,21 +14,26 @@ var ViewModel = function() {
         } else if (clicks < 10) {
             title = 'Infant';
         } else if (clicks < 25) {
-            title = 'Teen'
+            title = 'Teen';
         } else if (clicks < 50) {
-            title = 'Young Adult'
+            title = 'Young Adult';
         } else if (clicks < 75) {
-            title = 'Adult'
+            title = 'Adult';
         } else {
-            title = 'Pensioner'
+            title = 'Pensioner';
         }
         return title;
     }, this);
+};
 
 
+var ViewModel = function() {
+    var self = this;
+
+    this.currentCat = ko.observable( new Cat() );
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
-}
+};
 
 ko.applyBindings(new ViewModel());
